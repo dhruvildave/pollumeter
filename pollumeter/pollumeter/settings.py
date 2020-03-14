61abfb64a14710576b50ee6fe5a903a969b9e63d
Kaushal1011 <kaushalpatil10@gmail.com>
2020-03-14 20:13:39 +0530

    added graphql

--- pollumeter/pollumeter/settings.py
+++ pollumeter/pollumeter/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pollumeterbeta',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE={
+    'SCHEMA':'pollumeter.schema.schema',
+}
---
+++ pollumeter/pollumeterbeta/schema.py
@@ -0,0 +1,23 @@
+import graphene
+from graphene_django import DjangoObjectType
+from django.db.models import Q
+
+from .models import pollimetermodel
+
+class pollumeterType(DjangoObjectType):
+    class Meta:
+        model=pollimetermodel
+
+
+class Query(graphene.ObjectType):
+    
+    datapol=graphene.List(pollumeterType)
+
+    def resolve_datapol(self,info,numberrecords=None,startdatetime=None,enddatetime=None,**kwargs):
+        if startdatetime and enddatetime:
+            filter=(Q(datetime__gte=startdatetime, recordDate__lte=enddatetime))
+            return pollimetermodel.objects.filter(filter)
+        if numberrecords:
+            return pollimetermodel.objects.all()[-numberrecords]
+        return pollimetermodel.objects.all()
+        
