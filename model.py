a0b9ac6115604dc864e02104fdefe4d412382f76
Dhruvil Dave <dhruvil.dave@outlook.com>
2020-03-15 12:23:23 +0530

    Model complete

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import os
+
 import numpy as np
 import pandas as pd
 import tensorflow as tf
@@ -41,43 +43,52 @@ def create_model(data: np.ndarray) -> models.Sequential:
     return model
 
 
+def train() -> None:
+    pass
+
+
 def main() -> None:
-    df: pd.DataFrame = pd.read_csv('final_df.csv')
+    if not os.path.exists('model.h5'):
+        df: pd.DataFrame = pd.read_csv('final_df.csv')
 
-    df = df[['weather_temp', 'indpro', 'traf', 'aqi', 'weather_humidity']]
-    df['nextaqi'] = 0
-    df['next2aqi'] = 0
-    df['next3aqi'] = 0
-    df['next4aqi'] = 0
-    df['next5aqi'] = 0
-    df['next6aqi'] = 0
-    df['next7aqi'] = 0
-    df['next8aqi'] = 0
-    df['next9aqi'] = 0
-    df['next10aqi'] = 0
+        df = df[['weather_temp', 'indpro', 'traf', 'aqi', 'weather_humidity']]
+        df['nextaqi'] = 0
+        df['next2aqi'] = 0
+        df['next3aqi'] = 0
+        df['next4aqi'] = 0
+        df['next5aqi'] = 0
+        df['next6aqi'] = 0
+        df['next7aqi'] = 0
+        df['next8aqi'] = 0
+        df['next9aqi'] = 0
+        df['next10aqi'] = 0
 
-    df['SMAaqi'] = df.aqi.rolling(5).mean()
-    df['SMAaqi10'] = df.aqi.rolling(10).mean()
+        df['SMAaqi'] = df.aqi.rolling(5).mean()
+        df['SMAaqi10'] = df.aqi.rolling(10).mean()
 
-    # df = df.reshape(df.shape[0], df.shape[1], 1)
-    for i in range(len(df) - 11):
-        df['nextaqi'][i] = df['aqi'][i + 1]
-        df['next2aqi'][i] = df['aqi'][i + 2]
-        df['next3aqi'][i] = df['aqi'][i + 3]
-        df['next4aqi'][i] = df['aqi'][i + 4]
-        df['next5aqi'][i] = df['aqi'][i + 5]
-        df['next6aqi'][i] = df['aqi'][i + 6]
-        df['next7aqi'][i] = df['aqi'][i + 7]
-        df['next8aqi'][i] = df['aqi'][i + 8]
-        df['next9aqi'][i] = df['aqi'][i + 9]
-        df['next10aqi'][i] = df['aqi'][i + 10]
+        # df = df.reshape(df.shape[0], df.shape[1], 1)
+        for i in range(len(df) - 11):
+            df['nextaqi'][i] = df['aqi'][i + 1]
+            df['next2aqi'][i] = df['aqi'][i + 2]
+            df['next3aqi'][i] = df['aqi'][i + 3]
+            df['next4aqi'][i] = df['aqi'][i + 4]
+            df['next5aqi'][i] = df['aqi'][i + 5]
+            df['next6aqi'][i] = df['aqi'][i + 6]
+            df['next7aqi'][i] = df['aqi'][i + 7]
+            df['next8aqi'][i] = df['aqi'][i + 8]
+            df['next9aqi'][i] = df['aqi'][i + 9]
+            df['next10aqi'][i] = df['aqi'][i + 10]
 
-    df_train = df[:50000]
-    df_trainset = df_train[['indpro', 'traf']]
-    df_yset = df_train[['aqi', 'nextaqi', 'next2aqi', 'next3aqi', 'next4aqi']]
-    model = create_model(data=df_train)
-    history = model.fit(df_trainset, df_yset, epochs=10)
-    model.save('model.h5')
+        df_train = df[:50000]
+        df_trainset = df_train[['indpro', 'traf']]
+        df_yset = df_train[[
+            'aqi', 'nextaqi', 'next2aqi', 'next3aqi', 'next4aqi'
+        ]]
+        model = create_model(data=df_train)
+        history = model.fit(df_trainset, df_yset, epochs=10)
+        model.save('model.h5')
+    else:
+        model = models.load_model('model.h5')
 
     pred = model.predict(df_trainset)
     print(pred)
