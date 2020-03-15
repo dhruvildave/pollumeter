915f71620d5006ee01107676955b6118202442fc
Kaushal1011 <kaushalpatil10@gmail.com>
2020-03-15 08:52:56 +0530

    graphql connnected

--- pollumeter/src/App.js
+++ pollumeter/src/App.js
@@ -1,31 +1,28 @@
-import React, { Suspense } from 'react';
+import React, { Suspense } from "react";
 //  apollo
-import { ApolloProvider } from 'react-apollo'
-import { ApolloClient } from 'apollo-client'
-import { createHttpLink } from 'apollo-link-http'
-import { InMemoryCache } from 'apollo-cache-inmemory'
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 //rest
-import './App.css';
+import "./App.css";
 import { ThemeProvider } from "@material-ui/styles";
-import {
-  CssBaseline,
-  createMuiTheme
-} from "@material-ui/core";
+import { CssBaseline, createMuiTheme } from "@material-ui/core";
 
-import ResponsiveDrawer from './components/drawer'
-
+import ResponsiveDrawer from "./components/drawer";
 
 // 2
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000'
-})
+  uri: "http://localhost:8000/graphql/"
+});
 
 // 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
-})
-
+});
 
 const theme = createMuiTheme({
   palette: {
@@ -33,55 +30,119 @@ const theme = createMuiTheme({
   }
 });
 
+const FEED_QUERY = gql`
+  {
+    datapol(
+      area: "Foster"
+      startdatetime: "2017-11-18"
+      enddatetime: "2017-12-18"
+    ) {
+      datetime
+      polCo2
+      polSo2
+      polNo2
+      polCo
+      polPm10
+      polPm25
+      aqi
+      area
+    }
+  }
+`;
 class App extends React.Component {
   constructor(props) {
     super(props);
+    // try query
+    // try query end
     this.state = {
-      dates: { fromdate: new Date('2014-08-18'), todate: new Date('2018-08-18') },
-      options: [{ name: 'CO2', selected: true }, { name: 'SO2', selected: false }, { name: 'CO', selected: true }, { name: 'NO2', selected: false },],
-      categories: [{ name: 'A', range: 100, currentValue: 20 }, { name: 'B', range: '200', currentValue: 10 }, { name: 'C', range: 1000, currentValue: 100 }],
-      data_cat: ['uv', 'pv', 'amt',],
+      dates: {
+        fromdate: new Date("2014-08-18"),
+        todate: new Date("2018-08-18")
+      },
+      options: [
+        { name: "CO2", selected: true },
+        { name: "SO2", selected: false },
+        { name: "CO", selected: true },
+        { name: "NO2", selected: false }
+      ],
+      categories: [
+        { name: "A", range: 100, currentValue: 20 },
+        { name: "B", range: "200", currentValue: 10 },
+        { name: "C", range: 1000, currentValue: 100 }
+      ],
+      data_cat: [
+        "polCo2",
+        "polSo2",
+        "polNo2",
+        "polCo",
+        "polPm10",
+        "polPm25",
+        "aqi"
+      ],
       data: [
         {
-          date: '2014-08-18', uv: 4000, pv: 2400, amt: 2400, a: 1100,
+          date: "2014-08-18",
+          uv: 4000,
+          pv: 2400,
+          amt: 2400,
+          a: 1100
         },
         {
-          date: '2014-08-19', uv: 3000, pv: 1398, amt: 2210, a: 1200,
+          date: "2014-08-19",
+          uv: 3000,
+          pv: 1398,
+          amt: 2210,
+          a: 1200
         },
         {
-          date: '2014-08-20', uv: 2000, pv: 9800, amt: 2290, a: 1300,
+          date: "2014-08-20",
+          uv: 2000,
+          pv: 9800,
+          amt: 2290,
+          a: 1300
         },
         {
-          date: '2014-08-21', uv: 2780, pv: 3908, amt: 2000, a: 1100,
+          date: "2014-08-21",
+          uv: 2780,
+          pv: 3908,
+          amt: 2000,
+          a: 1100
         },
         {
-          date: '2014-08-22', uv: 1890, pv: 4800, amt: 2181, a: 1000,
+          date: "2014-08-22",
+          uv: 1890,
+          pv: 4800,
+          amt: 2181,
+          a: 1000
         },
         {
-          date: '2014-08-30', uv: 2390, pv: 3800, amt: 2500, a: 900,
-        },
-      ],
-    }
+          date: "2014-08-30",
+          uv: 2390,
+          pv: 3800,
+          amt: 2500,
+          a: 900
+        }
+      ]
+    };
   }
 
   toggleSelect(i) {
     this.setState(state => {
       state.options[i].selected = !state.options[i].selected;
       return state;
-    })
+    });
   }
   changeValue(index, newValue) {
     this.setState(state => {
       state.categories[index].currentValue = newValue;
       return state;
-    }
-    )
+    });
   }
   changeDate(type, date) {
     this.setState(state => {
       state.dates[type] = date;
-      return state
-    })
+      return state;
+    });
   }
   render() {
     return (
@@ -90,13 +151,32 @@ class App extends React.Component {
           <ThemeProvider theme={theme}>
             <Suspense fallback={"Loading"}>
               <CssBaseline />
-              <ResponsiveDrawer data_cat={this.state.data_cat} data={this.state.data} dates={this.state.dates} changeDates={this.changeDate.bind(this)} categories={this.state.categories} setValue={this.changeValue.bind(this)} options={this.state.options} toggleSelect={this.toggleSelect.bind(this)} />
+              <Query query={FEED_QUERY}>
+                {({ loading, error, data }) => {
+                  if (loading) return <div>Loading</div>;
+                  if (error) return <div>We Fucked Up</div>;
+
+                  console.log(data.datapol);
+                  return (
+                    <ResponsiveDrawer
+                      data_cat={this.state.data_cat}
+                      data={data.datapol}
+                      dates={this.state.dates}
+                      changeDates={this.changeDate.bind(this)}
+                      categories={this.state.categories}
+                      setValue={this.changeValue.bind(this)}
+                      options={this.state.options}
+                      toggleSelect={this.toggleSelect.bind(this)}
+                    />
+                  );
+                }}
+              </Query>
             </Suspense>
           </ThemeProvider>
-        </ApolloProvider>,
-
+        </ApolloProvider>
+        ,
       </>
-    )
+    );
   }
 }
 
--- pollumeter/src/components/drawer.jsx
+++ pollumeter/src/components/drawer.jsx
@@ -1,151 +1,167 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Divider from '@material-ui/core/Divider';
-import Drawer from '@material-ui/core/Drawer';
-import Hidden from '@material-ui/core/Hidden';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import ParaBlock from './parablock';
-import Graph from './graph';
-import Navigation from './nav';
-import DatePicker from './datepicker'
+import React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Divider from "@material-ui/core/Divider";
+import Drawer from "@material-ui/core/Drawer";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import ParaBlock from "./parablock";
+import Graph from "./graph";
+import Navigation from "./nav";
+import DatePicker from "./datepicker";
 const drawerWidth = 300;
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-    },
-    drawer: {
-        [theme.breakpoints.up('sm')]: {
-            width: drawerWidth,
-            flexShrink: 0,
-        },
-    },
-    appBar: {
-        [theme.breakpoints.up('sm')]: {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: drawerWidth,
-        },
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-        [theme.breakpoints.up('sm')]: {
-            display: 'none',
-        },
-    },
-    toolbar: theme.mixins.toolbar,
-    space: {
-        height: '3em',
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    content: {
-        marginTop: '10vh',
-        display: 'flex',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-        flexGrow: 1,
-        flexWrap: 'wrap',
-        padding: theme.spacing(3),
-    },
+  root: {
+    display: "flex"
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0
+    }
+  },
+  appBar: {
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none"
+    }
+  },
+  toolbar: theme.mixins.toolbar,
+  space: {
+    height: "3em"
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  content: {
+    marginTop: "10vh",
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    flexGrow: 1,
+    flexWrap: "wrap",
+    padding: theme.spacing(3)
+  }
 }));
 
 function ResponsiveDrawer(props) {
-    const { container } = props;
-    const classes = useStyles();
-    const theme = useTheme();
-    const [mobileOpen, setMobileOpen] = React.useState(false);
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-    const drawer = (
-        <div>
-            <div className={classes.space} />
-            <DatePicker date={props.dates.fromdate} type={"fromdate"} changeDates={props.changeDates} />
-            <DatePicker date={props.dates.todate} type={"todate"} changeDates={props.changeDates} />
-            <div className={classes.space} />
-            <Divider />
-            <div className={classes.space} />
-            <ParaBlock categories={props.categories} setValue={props.setValue} />
-        </div>
-    );
+  const drawer = (
+    <div>
+      <div className={classes.space} />
+      <DatePicker
+        date={props.dates.fromdate}
+        type={"fromdate"}
+        changeDates={props.changeDates}
+      />
+      <DatePicker
+        date={props.dates.todate}
+        type={"todate"}
+        changeDates={props.changeDates}
+      />
+      <div className={classes.space} />
+      <Divider />
+      <div className={classes.space} />
+      <ParaBlock categories={props.categories} setValue={props.setValue} />
+    </div>
+  );
 
-
-    return (
-        <div className={classes.root}>
-            <CssBaseline />
-            <AppBar position="fixed" className={classes.appBar}>
-                <Toolbar>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        className={classes.menuButton}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="subtitle1" noWrap>
-                        App
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="subtitle1" noWrap>
+            App
           </Typography>
-                </Toolbar>
-            </AppBar>
-            <nav className={classes.drawer} aria-label="mailbox folders">
-                {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-                <Hidden smUp implementation="css">
-                    <Drawer
-                        container={container}
-                        variant="temporary"
-                        anchor={theme.direction === 'rtl' ? 'right' : 'left'}
-                        open={mobileOpen}
-                        onClose={handleDrawerToggle}
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        ModalProps={{
-                            keepMounted: true, // Better open performance on mobile.
-                        }}
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-                <Hidden xsDown implementation="css">
-                    <Drawer
-                        classes={{
-                            paper: classes.drawerPaper,
-                        }}
-                        variant="permanent"
-                        open
-                    >
-                        {drawer}
-                    </Drawer>
-                </Hidden>
-            </nav>
-            <Navigation options={props.options} click={props.toggleSelect} />
-            <main className={classes.content}>
-                <Graph className={classes.content} data={props.data} data_cat={props.data_cat} />
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            ModalProps={{
+              keepMounted: true // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <Navigation options={props.options} click={props.toggleSelect} />
+      <main className={classes.content}>
+        <Graph
+          key={1}
+          className={classes.content}
+          data={props.data}
+          data_cat={["polCo2", "aqi"]}
+        />
 
-                <Graph className={classes.content} data={props.data} data_cat={props.data_cat} />
-
-            </main>
-        </div>
-    );
+        <Graph
+          key={2}
+          className={classes.content}
+          data={props.data}
+          data_cat={["polSo2", "polNo2", "polCo", "polPm10", "polPm25"]}
+        />
+      </main>
+    </div>
+  );
 }
 
 ResponsiveDrawer.propTypes = {
-    /**
-     * Injected by the documentation to work in an iframe.
-     * You won't need it on your project.
-     */
-    container: PropTypes.any,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  container: PropTypes.any
 };
 
 export default ResponsiveDrawer;
